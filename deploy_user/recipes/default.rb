5a504e7e6b84835053847efaf9d7158f8a8d8f55
James Miller <bensie@gmail.com>
2011-10-09 01:59:20 -0700

    Add custom deploy_user cookbook

---
+++ deploy_user/recipes/default.rb
@@ -0,0 +1,35 @@
+sysadmin_group = Array.new
+
+data_bag_item("deploy_user", "deploy_user") do |u|
+  sysadmin_group << u['id']
+
+  home_dir = "/home/#{u['id']}"
+
+  user u['id'] do
+    uid u['uid']
+    gid u['gid']
+    shell u['shell']
+    comment u['comment']
+    supports :manage_home => true
+    home home_dir
+  end
+
+  directory "#{home_dir}/.ssh" do
+    owner u['id']
+    group u['gid'] || u['id']
+    mode "0700"
+  end
+
+  template "#{home_dir}/.ssh/authorized_keys" do
+    source "authorized_keys.erb"
+    owner u['id']
+    group u['gid'] || u['id']
+    mode "0600"
+    variables :ssh_keys => u['ssh_keys']
+  end
+end
+
+group "sysadmin" do
+  gid 2300
+  members sysadmin_group
+end
