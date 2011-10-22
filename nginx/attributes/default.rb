309466a35f2cf99f71a7b945eaaf868963766276
James Miller <bensie@gmail.com>
2011-10-22 08:25:10 -0700

    Nginx 1.0.8

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:nginx][:version]            = "1.0.5"
+default[:nginx][:version]            = "1.0.8"
 
 set[:nginx][:dir]                    = "/etc/nginx"
 set[:nginx][:log_dir]                = "/var/log/nginx"
--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -2,13 +2,10 @@ maintainer        "Gerhard Lazu"
 maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "1.0.5"
+version           "1.0.8"
 
 recipe "nginx", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
 recipe "nginx::source", "Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available"
 
 supports "ubuntu"
 supports "debian"
-
-depends "build-essential"
-depends "apt"
