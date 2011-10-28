7b256a481e9efaccbb45ddfdd1a65f239b30fea3
James Miller <bensie@gmail.com>
2011-10-28 15:14:36 -0700

    Invalid version

--- jetty/metadata.rb
+++ jetty/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "theo@jivatechnology.com"
 license          "Apache 2.0"
 description      "Installs/Configures jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "7.5.4.v20111024"
+version          "7.5.4" #v20111024
