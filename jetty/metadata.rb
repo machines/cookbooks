a1680ca9417a7ace392ae33387aec12ce08af399
James Miller <bensie@gmail.com>
2011-10-28 15:11:13 -0700

    Add initial jetty cookbook

---
+++ jetty/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Jiva Technology Limited"
+maintainer_email "theo@jivatechnology.com"
+license          "Apache 2.0"
+description      "Installs/Configures jetty"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "7.5.4.v20111024"
