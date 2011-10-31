123612fbfe53ad0ed6f06e20621512e829174869
James Miller <bensie@gmail.com>
2011-10-31 13:23:01 -0700

    Dumb typi

--- application/recipes/rails.rb
+++ application/recipes/rails.rb
@@ -160,7 +160,7 @@ node.run_state[:rails_apps].each do |app|
         cwd release_path
         user app['owner']
         group app['group']
-        code "(bundle install --deployment --without #{(common_groups -([app['environment']])).join(' ')} --binstubs --shebang ruby-local-exec"
+        code "bundle install --deployment --without #{(common_groups -([app['environment']])).join(' ')} --binstubs --shebang ruby-local-exec"
       end
     end
 
