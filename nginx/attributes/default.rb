f2bfd620e0d9616214b41932601a369b1c25ecb0
James Miller <bensie@gmail.com>
2012-01-16 19:26:12 -0800

    Bump nginx version

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:nginx][:version]            = "1.0.10"
+default[:nginx][:version]            = "1.0.11"
 
 set[:nginx][:dir]                    = "/etc/nginx"
 set[:nginx][:log_dir]                = "/var/log/nginx"
