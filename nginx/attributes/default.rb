e676de7624a86195f9cb8665c90fe20e194877cc
James Miller <bensie@gmail.com>
2012-04-16 11:35:10 -0700

    Not a local var dummy

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -41,9 +41,9 @@ default[:nginx][:gzip_types] = [
 
 # Optional Phusion Passenger module
 default[:nginx][:passenger][:revision] = "1afc4dea90"
-default[:nginx][:passenger][:url] = "http://virtmachine.s3.amazonaws.com/passenger/passenger-3.2.0.#{revision}.tgz"
+default[:nginx][:passenger][:url] = "http://virtmachine.s3.amazonaws.com/passenger/passenger-3.2.0.#{nginx.passenger.revision}.tgz"
 default[:nginx][:passenger][:checksum] = "a6fcfc4b131d6a8d65166734b15bf501590565b7e9723335d9f3e8d5f51c4ec8"
 default[:nginx][:passenger][:enabled] = true
 default[:nginx][:passenger][:root] = "/usr/local/src/passenger"
-default[:nginx][:passenger][:nginx_module_path] = File.join(nginx[:passenger][:root], 'ext/nginx')
+default[:nginx][:passenger][:nginx_module_path] = File.join(nginx.passenger.root, 'ext/nginx')
 default[:nginx][:passenger][:max_pool_size] = 25
