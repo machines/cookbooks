0575ef90f7f8eb46fdf2d90496438f5b8117b32f
James Miller <bensie@gmail.com>
2012-05-29 12:57:09 -0700

    Log monit to rsyslog

--- monit/recipes/default.rb
+++ monit/recipes/default.rb
@@ -1,5 +1,13 @@
 package "monit"
 
+template "/etc/rsyslog.d/monit.conf" do
+  source "monit.rsyslog.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, "service[rsyslog]", :delayed
+end
+
 template "monitrc" do
   path "/etc/monit/monitrc"
   source "monitrc.erb"
