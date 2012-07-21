3dc26f6138b739e5d009d46efff818ce9a153370
James Miller <bensie@gmail.com>
2012-07-21 16:30:20 -0700

    Roll back passenger a bit

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -46,7 +46,7 @@ default[:nginx][:gzip_types] = [
 
 # Optional Phusion Passenger module
 default[:nginx][:passenger][:enabled]               = true
-default[:nginx][:passenger][:git_revision]          = "e91b8b68c699f582dc8a61e150bbeacb53b0e06d"
+default[:nginx][:passenger][:git_revision]          = "0d9d9bc83ad07a38ba1852d1f2c7e6ff4efe0c47"
 default[:nginx][:passenger][:root]                  = "/usr/local/src/passenger-#{nginx.passenger.git_revision}"
 default[:nginx][:passenger][:nginx_module_path]     = File.join(nginx.passenger.root, 'ext/nginx')
 default[:nginx][:passenger][:log_level]             = 0
