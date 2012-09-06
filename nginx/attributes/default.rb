fe99544e5686adf5d0038fc49eba22237f1326f3
James Miller <bensie@gmail.com>
2012-09-05 22:37:39 -0700

    Update Passenger
    Commit 72f9473edd461f4d6b6d05387bc3ed6d2cd0e095

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -46,7 +46,7 @@ default[:nginx][:gzip_types] = [
 
 # Optional Phusion Passenger module
 default[:nginx][:passenger][:enabled]               = true
-default[:nginx][:passenger][:git_revision]          = "3282f3ea3a9aa478d87e789aa0f03863b8b7127a"
+default[:nginx][:passenger][:git_revision]          = "72f9473edd461f4d6b6d05387bc3ed6d2cd0e095"
 default[:nginx][:passenger][:root]                  = "/usr/local/src/passenger-#{nginx.passenger.git_revision}"
 default[:nginx][:passenger][:nginx_module_path]     = File.join(nginx.passenger.root, 'ext/nginx')
 default[:nginx][:passenger][:log_level]             = 0
