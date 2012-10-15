bb0994f4f4e90461376dbb3895b672b78190a823
James Miller <bensie@gmail.com>
2012-10-15 09:04:38 -0700

    Add 1.9.3-p286 and make default, add 1.8.7-p370

--- rubies/attributes/default.rb
+++ rubies/attributes/default.rb
@@ -1,9 +1,9 @@
 default[:rubies][:versions] = [
-  "1.9.3-p194", "1.9.3-p125", "1.9.3-p0", "1.9.2-p320", "1.9.2-p290", "1.8.7-p358", "ree-1.8.7-2012.02"
+  "1.9.3-p286", "1.9.3-p194", "1.9.3-p125", "1.9.3-p0", "1.9.2-p320", "1.9.2-p290", "1.8.7-p370", "1.8.7-p358", "ree-1.8.7-2012.02"
 ]
 default[:rubies][:source]              = "http://packages.machines.io/rubies"
 default[:rubies][:install_path]        = "/opt/rubies"
-default[:rubies][:system_ruby_version] = "1.9.3-p194"
+default[:rubies][:system_ruby_version] = "1.9.3-p286"
 default[:rubies][:rbenv_path]          = "/usr/local/rbenv"
 
 # Gems installed on all rubies
@@ -13,7 +13,7 @@ default[:rubies][:gems] = [
 
 # Gems installed to the default system ruby
 default[:rubies][:system_ruby_gems] = [
-  ["chef", "10.14.2"],
+  ["chef", "10.14.4"],
   ["god", "0.13.1"],
   ["backup", "3.0.25"],
   ["remote_syslog", "1.6.7.2"]
