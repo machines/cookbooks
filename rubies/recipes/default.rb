902a17063f4d74f3d7604c38722e2bed2d38e4ff
James Miller <bensie@gmail.com>
2012-11-30 19:34:40 -0800

    Inline the env

--- rubies/recipes/default.rb
+++ rubies/recipes/default.rb
@@ -26,9 +26,8 @@ node.rubies.versions.each do |ruby|
   gems.each do |g|
     bash "install #{g[0]} #{g[1]} to #{ruby}" do
       cwd "/opt/rubies/#{ruby}/bin"
-      code "./gem install #{g[0]} -v #{g[1]} --no-rdoc --no-ri"
-      not_if %(./gem list --local | grep -E "(#{g[0]})(.+)(#{g[1]})"), cwd: "/opt/rubies/#{ruby}/bin"
-      environment { "RBENV_VERSION" => "#{ruby}" }
+      code "RBENV_VERSION=#{ruby} ./gem install #{g[0]} -v #{g[1]} --no-rdoc --no-ri"
+      not_if %(RBENV_VERSION=#{ruby} ./gem list --local | grep -E "(#{g[0]})(.+)(#{g[1]})"), cwd: "/opt/rubies/#{ruby}/bin"
     end
   end
 
