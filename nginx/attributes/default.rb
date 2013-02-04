1e9e2817234543c0ef0ec0f15cf238f62371c1cd
James Miller <bensie@gmail.com>
2013-02-04 11:28:36 -0800

    Bump Passenger revision, commit 3c92e045dacf3db2e05fc44f069239953dde82b3

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -46,7 +46,7 @@ default[:nginx][:gzip_types] = [
 
 # Optional Phusion Passenger module
 default[:nginx][:passenger][:enabled]               = true
-default[:nginx][:passenger][:git_revision]          = "a6b90cad6e800920255781af14b48c0e1d476c0c"
+default[:nginx][:passenger][:git_revision]          = "3c92e045dacf3db2e05fc44f069239953dde82b3"
 default[:nginx][:passenger][:root]                  = "/usr/local/src/passenger-#{nginx.passenger.git_revision}"
 default[:nginx][:passenger][:nginx_module_path]     = File.join(nginx.passenger.root, 'ext/nginx')
 default[:nginx][:passenger][:log_level]             = 0
