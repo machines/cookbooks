6cddcaf6221e94258ff3a46f8f1f0c8a15cb79a7
James Miller <bensie@gmail.com>
2013-02-21 15:27:58 -0800

    Ruby Updates, remove god
    * Add 2.0.0-rc2
    * Add 1.9.3-p385
    * Add JRuby 1.7.3
    * Remove JRuby 1.7.2 (security issues)
    * Remove 1.9.3-p374 (security issues)
    * Make 1.9.3-p385 default

--- rubies/attributes/default.rb
+++ rubies/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:rubies][:versions] = [
-  "2.0.0-rc1",
-  "1.9.3-p374",
+  "2.0.0-rc2",
+  "1.9.3-p385",
   "1.9.3-p327",
   "1.9.3-p286",
   "1.9.3-p194",
@@ -8,11 +8,11 @@ default[:rubies][:versions] = [
   "1.9.2-p290",
   "1.8.7-p371",
   "1.8.7-p358",
-  "jruby-1.7.2"
+  "jruby-1.7.3"
 ]
 default[:rubies][:source]              = "http://packages.machines.io/rubies"
 default[:rubies][:install_path]        = "/opt/rubies"
-default[:rubies][:system_ruby_version] = "1.9.3-p374"
+default[:rubies][:system_ruby_version] = "1.9.3-p385"
 default[:rubies][:rbenv_path]          = "/usr/local/rbenv"
 
 # Gems installed on all rubies
@@ -23,7 +23,6 @@ default[:rubies][:gems] = [
 # Gems installed to the default system ruby
 default[:rubies][:system_ruby_gems] = [
   ["chef", "11.4.0"],
-  ["god", "0.13.1"],
   ["backup", "3.0.27"],
   ["remote_syslog", "1.6.9"]
 ]
