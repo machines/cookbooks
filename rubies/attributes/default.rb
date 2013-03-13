ca560f3a00b321402957ed96197a262320bb6533
James Miller <bensie@gmail.com>
2013-03-13 10:18:59 -0700

    Bundler 1.3.3, Backup 3.1.2

--- rubies/attributes/default.rb
+++ rubies/attributes/default.rb
@@ -16,12 +16,12 @@ default[:rubies][:rbenv_path]          = "/usr/local/rbenv"
 
 # Gems installed on all rubies
 default[:rubies][:gems] = [
-  ["bundler", "1.3.0"]
+  ["bundler", "1.3.3"]
 ]
 
 # Gems installed to the default system ruby
 default[:rubies][:system_ruby_gems] = [
   ["chef", "11.4.0"],
-  ["backup", "3.0.27"],
+  ["backup", "3.1.2"],
   ["remote_syslog", "1.6.9"]
 ]
