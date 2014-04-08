ab5e1cc6da8c565a6fd673f18cda4335f06d9663
James Miller <bensie@gmail.com>
2014-04-08 10:41:04 -0700

    Passenger 4.0.41

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -46,7 +46,7 @@ default[:nginx][:gzip_types] = [
 
 # Optional Phusion Passenger module
 default[:nginx][:passenger][:enabled]               = true
-default[:nginx][:passenger][:git_revision]          = "34b1087870c2bf85ebfd72c30b78577e10ab9744"
+default[:nginx][:passenger][:git_revision]          = "80e305fc6d560288e6a93e63d8799898c2fdbf41"
 default[:nginx][:passenger][:root]                  = "/usr/local/src/passenger-#{nginx.passenger.git_revision}"
 default[:nginx][:passenger][:nginx_module_path]     = File.join(nginx.passenger.root, 'ext/nginx')
 default[:nginx][:passenger][:log_level]             = 0
